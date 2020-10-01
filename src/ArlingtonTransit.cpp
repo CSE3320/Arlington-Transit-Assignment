4aca79966afa5d5065d8b5bb5da2fa7d9a113cdf
Trevor Bakker <trevor.bakker@uta.edu>
2020-10-01 19:46:59 +0000

    Small update

--- src/ArlingtonTransit.cpp
+++ src/ArlingtonTransit.cpp
@@ -5,8 +5,24 @@
 
 int main()
 {
-  Station station(StationName::UTA);
-  Station station(StationName::Prince);
+  Station Uta(StationName::UTA);
+  Station Prince(StationName::Prince);
+
+  std::vector< Edge > UTA_Line;
+
+  struct Edge UTA2Prince;
+  UTA2Prince . src  = StationName::UTA;
+  UTA2Prince . dest = StationName::Prince;
+  UTA2Prince . distance = 1;
+
+  UTA_Line . push_back( UTA2Prince );
+
+  TransitLine UTA( UTA_Line, 2 );
+
+  std::cout << UTA << std::endl;
+
+
+  
   return 0;
 }
 
